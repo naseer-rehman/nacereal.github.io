e838374a807579bb93f060e72a7c6795a3966c7c
Naseer Rehman <rehmn.nas@gmail.com>
2024-06-20 10:42:31 -0400

    Change folder structure and styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "../styles/Header.scss";
+
+function Header() {
+  return (
+    <h1 className="header">naseer-rehman@mywebsite<span className="header__colon">:</span><span className="header__tilda">~</span><span className="header__dollar">$</span></h1>
+  );
+}
+
+export default Header;
