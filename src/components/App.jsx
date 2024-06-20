587b588c5f389cda6cb03f85b3ea8aef7e0fdbfa
Naseer Rehman <rehmn.nas@gmail.com>
2024-06-20 19:17:05 -0400

    Add header cursor animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
 import '../styles/App.scss'
+import { useState, useEffect, useRef } from "react";
 import Header from "./Header";
+import isUserOnMobile from "../modules/checkMobile.js";
 
 function App() {
-  
+  // TODO: use something like this for the animation
+  const [showAboutSection, setShowAboutSection] = useState(false); 
+  // TODO: Fix this? Showing YES on desktop
+  // const isOnMobile = useRef(isUserOnMobile()); 
+
+  // TODO: Clean this up
+  useEffect(() => {
+    // console.log(`Loaded! Mobile ${isOnMobile ? "YES" : "NO"}`);
+
+    return () => {
+      // console.log("No longer loaded!");
+    };
+  }, []);
+
 
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,14 @@ import "../styles/Header.scss";
 
 function Header() {
   return (
-    <h1 className="header">naseer-rehman@mywebsite<span className="header__colon">:</span><span className="header__tilda">~</span><span className="header__dollar">$</span></h1>
+    <h1 className="header" aria-label="Naseer Rehman">
+      naseer-rehman@mywebsite
+      <span className="header__colon">:</span>
+      <span className="header__tilda">~</span>
+      <span className="header__dollar">$</span>
+      <span className="header__command-text"></span>
+      <span className="header__cursor header__cursor--flashing" />
+    </h1>
   );
 }
 
